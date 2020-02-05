641c67bcd83534a62e3c4c32d05deae8c0f396f6
shamu3018 <60501179+shamu3018@users.noreply.github.com>
2020-02-05 10:24:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function todoList()
+{
+	var item=document.getElementById("todoInput").value
+	var text=document.createTextNode(item)
+	var newItem=document.createElement("li")
+	newItem.appendChild(text)
+	document.getElementById("todoList").appendChild(newItem)
+}
